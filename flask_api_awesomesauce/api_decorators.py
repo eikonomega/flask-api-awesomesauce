7562c5115c27d38bc6c0643c783bba3dd1f79920
Mike Dunn <mike@eikonomega.com>
2014-07-16 14:00:57 -0400

    Extract python_to_json_syntax() from api_decorators -> utilities

--- flask_api_awesomesauce/api_decorators.py
+++ flask_api_awesomesauce/api_decorators.py
@@ -9,28 +9,7 @@ import re
 from flask import Response, jsonify, current_app, request
 from flask.wrappers import BadRequest
 
-
-def python_to_json_syntax(dict_to_convert):
-    """
-    Recursively update pythonic keys to JSON syntax in a dictionary
-    and nested dictionaries if present.
-
-    Args:
-        dict_to_convert (dict): Dictionary with keys to convert from
-            python to JSON syntax.
-
-    """
-    for key, value in dict_to_convert.iteritems():
-        old_key = key
-        for python_syntax in re.finditer(r'_[a-z]', key):
-                key = key.replace(
-                    python_syntax.group(), python_syntax.group()[1].upper())
-        dict_to_convert[key] = dict_to_convert.pop(old_key)
-
-        # Recursive call for nested dictionaries
-        if type(value) == dict:
-            python_to_json_syntax(value)
-
+import utilities
 
 
 def api_declaration(
---
+++ flask_api_awesomesauce/utilities.py
@@ -0,0 +1,26 @@
+"""
+This module contains utility methods used by other parts of the
+application.
+
+"""
+
+def python_to_json_syntax(dict_to_convert):
+    """
+    Recursively update pythonic keys to JSON syntax in a dictionary
+    and nested dictionaries if present.
+
+    Args:
+        dict_to_convert (dict): Dictionary with keys to convert from
+            python to JSON syntax.
+
+    """
+    for key, value in dict_to_convert.iteritems():
+        old_key = key
+        for python_syntax in re.finditer(r'_[a-z]', key):
+                key = key.replace(
+                    python_syntax.group(), python_syntax.group()[1].upper())
+        dict_to_convert[key] = dict_to_convert.pop(old_key)
+
+        # Recursive call for nested dictionaries
+        if type(value) == dict:
+            python_to_json_syntax(value)
