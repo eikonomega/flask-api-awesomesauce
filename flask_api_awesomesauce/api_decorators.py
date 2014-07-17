cbc921cd935d53105835a99df5e57f0098c355b5
Mike Dunn <mike@eikonomega.com>
2014-07-17 09:15:06 -0400

    Add unit tests covering basic functionality.

--- flask_api_awesomesauce/api_decorators.py
+++ flask_api_awesomesauce/api_decorators.py
@@ -3,16 +3,14 @@ Decorator functions for web api methods.
 """
 
 from functools import wraps
-import json
-import re
 
-from flask import Response, jsonify, current_app, request
+from flask import jsonify, request
 from flask.wrappers import BadRequest
 
 import utilities
 
 
-def json_response(status_code, response_data):
+def json_response(status_code, message, additional_data=None):
     """
     Args:
         status_code (int): HTTP Status Code for response object.
@@ -22,7 +20,17 @@ def json_response(status_code, response_data):
         A Flask Response object
 
     """
-    response = jsonify(response_data)
+    response_payload = {
+        'statusCode': status_code,
+        'message': message
+    }
+
+    if additional_data:
+        response_payload.update(additional_data)
+
+    utilities.python_to_json_syntax(response_payload)
+
+    response = jsonify(response_payload)
     response.status_code = status_code
     return response
 
@@ -60,8 +68,8 @@ def api_declaration(
                 request_json = request.get_json()
             except BadRequest:
                 return json_response(
-                    400, {'message': 'JSON is request has a syntax error(s)',
-                          'usage': api_usage_declarations})
+                    400, 'JSON request payload has a syntax error(s).',
+                    {'usage': api_usage_declarations})
 
             if 'requiredPayloadElements' in api_usage_declarations:
                 for element in api_usage_declarations[
@@ -69,137 +77,134 @@ def api_declaration(
                     if not element in request_json:
                         return json_response(
                             400,
-                            {'message':
-                                 'JSON is missing a required element(s).',
-                             'usage': api_usage_declarations})
+                            'JSON is missing a required element(s).',
+                            {'usage': api_usage_declarations})
 
-            elif 'requiredUrlParameters' in api_usage_declarations:
+            if 'requiredUrlParameters' in api_usage_declarations:
                 request_args = request.args
                 for element in api_usage_declarations['requiredUrlParameters']:
                     if not element in request_args:
                         return json_response(
                             400,
-                            {'message':
-                                 'One or more required URL '
-                                 'parameters are missing.',
-                             'usage': api_usage_declarations})
+                            'A required URL parameter(s) is missing.',
+                            {'usage': api_usage_declarations})
 
-            func(*args, **kwargs)
+            return func(*args, **kwargs)
 
         return wrapper
 
     return decorate
 
 
-def response_template(api_version,
-                      required_payload_elements=None,
-                      optional_payload_elements=None,
-                      required_url_parameters=None,
-                      optional_url_parameters=None):
-    """
-    Ensure that a flask.Response object conforms to desired specifications.
-
-    What should this function do?
-
-    On the front side:
-        1. Verifies that the JSON in the request in valid.
-        2. Verifies that required payload/url elements are provided.
-
-    On the backside:
-        1. Only include this information if original functions response code
-        is 4xx/5xx to avoid overly verbose responses.
-
-    """
-    # Extract dictionary objects from locals().items()
-    api_usage_declarations = {
-        key: value for key, value in locals().items() if type(value) == dict}
-
-    json_syntax_api_usage_declarations = dict()
-    for key, value in api_usage_declarations.iteritems():
-        for python_syntax in re.finditer(r'_[a-z]', key):
-            key = key.replace(
-                python_syntax.group(), python_syntax.group()[1].upper())
-        json_syntax_api_usage_declarations[key] = value
-
-    def decorate(func):
-        @wraps(func)
-        def wrapper(*args, **kwargs):
-
-            response_data = dict(
-                meta=dict(apiVersion=api_version,
-                          usage=json_syntax_api_usage_declarations,
-                          request=dict(
-                              requestUrl=request.base_url,
-                              requestMethod=request.method),
-                          response=dict()))
-
-            # Check for Invalid JSON.
-            try:
-                request.get_json()
-            except BadRequest:
-                response_data['meta']['response']['message'] = (
-                    'JSON contains syntax errors.')
-                response_data['meta']['response']['statusCode'] = 400
-                response = jsonify(response_data)
-                response.status_code = (
-                    response_data['meta']['response']['statusCode'])
-                current_app.logger.error(
-                    response_data['meta']['response']['message'])
-                return response
-
-            if 'requiredPayloadElements' in json_syntax_api_usage_declarations:
-                request_json = request.get_json()
-                for element in json_syntax_api_usage_declarations[
-                    'requiredPayloadElements']:
-                    if not element in request_json:
-                        response_data['meta']['response']['statusCode'] = 400
-                        response_data['meta']['response']['message'] = (
-                            'Required JSON element(s) are not in payload.')
-                        response = jsonify(response_data)
-                        response.status_code = (
-                            response_data['meta']['response']['statusCode'])
-                        current_app.logger.error(
-                            response_data['meta']['response']['message'])
-                        return response
-            elif 'requiredUrlParameters' in json_syntax_api_usage_declarations:
-                request_args = request.args
-                for element in json_syntax_api_usage_declarations[
-                    'requiredUrlParameters']:
-                    if not element in request_args:
-                        response_data['meta']['response']['statusCode'] = 400
-                        response_data['meta']['response']['message'] = (
-                            'Missing required URL parameter(s).')
-                        response = jsonify(response_data)
-                        response.status_code = (
-                            response_data['meta']['response']['statusCode'])
-                        current_app.logger.error(
-                            response_data['meta']['response']['message'])
-                        return response
-
-            original_response = func(*args, **kwargs)
-            original_response_data = json.loads(original_response.data)
-
-            if type(original_response) != Response:
-
-
-            # Reassign dict location of 'meta' elements from original response.
-            response_data['meta']['response'] = dict()
-            for meta_element in ['statusCode', 'message']:
-                if meta_element in original_response_data:
-                    response_data['meta']['response'][meta_element] = (
-                        original_response_data[meta_element])
-                    original_response_data.pop(meta_element)
-
-            # Transfer remaining origin_response elements to new response dict.
-            for element in original_response_data:
-                response_data[element] = original_response_data[element]
-
-            response = jsonify(response_data)
-            response.status_code = (
-                response_data['meta']['response']['statusCode'])
-            return response
-
-        return wrapper
-
-    return decorate
+# def response_template(api_version,
+#                       required_payload_elements=None,
+#                       optional_payload_elements=None,
+#                       required_url_parameters=None,
+#                       optional_url_parameters=None):
+#     """
+#     Ensure that a flask.Response object conforms to desired specifications.
+#
+#     What should this function do?
+#
+#     On the front side:
+#         1. Verifies that the JSON in the request in valid.
+#         2. Verifies that required payload/url elements are provided.
+#
+#     On the backside:
+#         1. Only include this information if original functions response code
+#         is 4xx/5xx to avoid overly verbose responses.
+#
+#     """
+#     # Extract dictionary objects from locals().items()
+#     api_usage_declarations = {
+#         key: value for key, value in locals().items() if type(value) == dict}
+#
+#     json_syntax_api_usage_declarations = dict()
+#     for key, value in api_usage_declarations.iteritems():
+#         for python_syntax in re.finditer(r'_[a-z]', key):
+#             key = key.replace(
+#                 python_syntax.group(), python_syntax.group()[1].upper())
+#         json_syntax_api_usage_declarations[key] = value
+#
+#     def decorate(func):
+#         @wraps(func)
+#         def wrapper(*args, **kwargs):
+#
+#             response_data = dict(
+#                 meta=dict(apiVersion=api_version,
+#                           usage=json_syntax_api_usage_declarations,
+#                           request=dict(
+#                               requestUrl=request.base_url,
+#                               requestMethod=request.method),
+#                           response=dict()))
+#
+#             # Check for Invalid JSON.
+#             try:
+#                 request.get_json()
+#             except BadRequest:
+#                 response_data['meta']['response']['message'] = (
+#                     'JSON contains syntax errors.')
+#                 response_data['meta']['response']['statusCode'] = 400
+#                 response = jsonify(response_data)
+#                 response.status_code = (
+#                     response_data['meta']['response']['statusCode'])
+#                 current_app.logger.error(
+#                     response_data['meta']['response']['message'])
+#                 return response
+#
+#             if 'requiredPayloadElements' in json_syntax_api_usage_declarations:
+#                 request_json = request.get_json()
+#                 for element in json_syntax_api_usage_declarations[
+#                     'requiredPayloadElements']:
+#                     if not element in request_json:
+#                         response_data['meta']['response']['statusCode'] = 400
+#                         response_data['meta']['response']['message'] = (
+#                             'Required JSON element(s) are not in payload.')
+#                         response = jsonify(response_data)
+#                         response.status_code = (
+#                             response_data['meta']['response']['statusCode'])
+#                         current_app.logger.error(
+#                             response_data['meta']['response']['message'])
+#                         return response
+#             elif 'requiredUrlParameters' in json_syntax_api_usage_declarations:
+#                 request_args = request.args
+#                 for element in json_syntax_api_usage_declarations[
+#                     'requiredUrlParameters']:
+#                     if not element in request_args:
+#                         response_data['meta']['response']['statusCode'] = 400
+#                         response_data['meta']['response']['message'] = (
+#                             'Missing required URL parameter(s).')
+#                         response = jsonify(response_data)
+#                         response.status_code = (
+#                             response_data['meta']['response']['statusCode'])
+#                         current_app.logger.error(
+#                             response_data['meta']['response']['message'])
+#                         return response
+#
+#             original_response = func(*args, **kwargs)
+#             original_response_data = json.loads(original_response.data)
+#
+#             if type(original_response) != Response:
+#
+#
+#             # Reassign dict location of 'meta' elements from original response.
+#             response_data['meta']['response'] = dict()
+#             for meta_element in ['statusCode', 'message']:
+#                 if meta_element in original_response_data:
+#                     response_data['meta']['response'][meta_element] = (
+#                         original_response_data[meta_element])
+#                     original_response_data.pop(meta_element)
+#
+#             # Transfer remaining origin_response elements to new response dict.
+#             for element in original_response_data:
+#                 response_data[element] = original_response_data[element]
+#
+#             response = jsonify(response_data)
+#             response.status_code = (
+#                 response_data['meta']['response']['statusCode'])
+#             return response
+#
+#         return wrapper
+#
+#     return decorate
 
--- flask_api_awesomesauce/tests/test_api_decorators.py
+++ flask_api_awesomesauce/tests/test_api_decorators.py
@@ -1,9 +1,10 @@
-from base64 import b64encode
+import json
 
 import pytest
 from flask import Flask, jsonify
 
-from flask_authorization_panda import basic_auth
+from flask_api_awesomesauce.api_decorators import (
+    api_declaration, json_response)
 
 
 @pytest.fixture
@@ -11,42 +12,107 @@ def flask_app():
     app = Flask(__name__)
     app.config['TESTING'] = True
 
-    @app.route('/')
-    @basic_auth
-    def hello_world():
-        return jsonify({"statusCode": 200, "message": "Ok"})
+    @app.route('/', methods=['GET'])
+    @api_declaration('1.0')
+    def basic_get_request():
+        return json_response(200, "Ok")
+
+    @app.route('/test-url-parameters', methods=['GET'])
+    @api_declaration('1.0', required_url_parameters={
+        "test_url_parameters": "Misc Value"})
+    def parameterized_get_request():
+        return json_response(200, "Ok")
+
+    @app.route('/test-post', methods=['POST'])
+    @api_declaration('1.0', required_payload_elements={
+        "test_element": "This is just a description"})
+    def json_post_request():
+        return json_response(201, "Ok")
 
     return app
 
-def test_no_credentials_in_application_config(flask_app):
-    response = flask_app.test_client().get('/', headers={
-        'Authorization': 'Basic {}'.format(b64encode('admin:secret'))})
-    assert '500' in response.data
+
+def test_api_declaration_with_no_optional_parameters(flask_app):
+    """
+    Unknown
+
+    """
+    response = json.loads(flask_app.test_client().get('/').get_data())
+    assert all([response['message'] == 'Ok', response['statusCode'] == 200])
 
 
-def test_no_credentials_in_request(flask_app):
-    flask_app.config['basic_auth_credentials'] = dict(username='admin',
-                                                      password='secret')
-    response = flask_app.test_client().get('/')
-    assert "HTTP Basic Auth required for this URL" in response.data
-    assert "WWW-Authenticate" in response.headers
+def test_api_declaration_with_missing_invalid_json_payload(flask_app):
+    """
+    Unknown
+
+    """
+    response = flask_app.test_client().post(
+        '/test-post',
+        content_type='application/json',
+        data="{'body': }")
+
+    response = json.loads(response.get_data())
+    assert all([
+        response['message'] == 'JSON request payload has a syntax error(s).',
+        response['statusCode'] == 400,
+        response['usage']])
 
 
-def test_basic_auth_with_bad_credentials(flask_app):
-    flask_app.config['basic_auth_credentials'] = dict(username='admin',
-                                                      password='secret')
-    response = flask_app.test_client().get('/', headers={
-        'Authorization': 'Basic {}'.format(b64encode('wrong:creds'))})
-    assert 'with proper credentials' in response.data
-    assert "WWW-Authenticate" in response.headers
+def test_api_declaration_with_missing_required_json_element(flask_app):
+    """
+    Unknown
+
+    """
+    response = flask_app.test_client().post(
+        '/test-post',
+        content_type='application/json',
+        data=json.dumps({'body': 'body of the blog post'}))
+
+    response = json.loads(response.get_data())
+    assert all([
+        response['message'] == 'JSON is missing a required element(s).',
+        response['statusCode'] == 400,
+        response['usage']])
 
 
-def test_basic_auth(flask_app):
-    flask_app.config['basic_auth_credentials'] = dict(username='admin',
-                                                      password='secret')
-    response = flask_app.test_client().get('/', headers={
-        'Authorization': 'Basic {}'.format(b64encode('admin:secret'))})
-    assert '200' in response.data
+def test_api_declaration_with_missing_required_url_parameter(flask_app):
+    """
+    Unknown
+
+    """
+    response = flask_app.test_client().get(
+        '/test-url-parameters')
+
+    response = json.loads(response.get_data())
+    print(response)
+    assert all([
+        response['message'] == 'A required URL parameter(s) is missing.',
+        response['statusCode'] == 400,
+        response['usage']])
+
+# def test_no_credentials_in_request(flask_app):
+#     flask_app.config['basic_auth_credentials'] = dict(username='admin',
+#                                                       password='secret')
+#     response = flask_app.test_client().get('/')
+#     assert "HTTP Basic Auth required for this URL" in response.data
+#     assert "WWW-Authenticate" in response.headers
+#
+#
+# def test_basic_auth_with_bad_credentials(flask_app):
+#     flask_app.config['basic_auth_credentials'] = dict(username='admin',
+#                                                       password='secret')
+#     response = flask_app.test_client().get('/', headers={
+#         'Authorization': 'Basic {}'.format(b64encode('wrong:creds'))})
+#     assert 'with proper credentials' in response.data
+#     assert "WWW-Authenticate" in response.headers
+#
+#
+# def test_basic_auth(flask_app):
+#     flask_app.config['basic_auth_credentials'] = dict(username='admin',
+#                                                       password='secret')
+#     response = flask_app.test_client().get('/', headers={
+#         'Authorization': 'Basic {}'.format(b64encode('admin:secret'))})
+#     assert '200' in response.data
 
 
 
